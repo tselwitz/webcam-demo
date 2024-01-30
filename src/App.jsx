ef09a133695bea5260d917034f3bf98188e547c2
Todd Selwitz <selwitz.todd@gmail.com>
2024-01-30 12:57:24 -0700

    Moves code to git repo

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { useState, useRef } from "react";
+import VideoRecorder from "../src/components/VideoRecorder";
+import AudioRecorder from "../src/components/AudioRecorder";
+
+const App = () => {
+  let [recordOption, setRecordOption] = useState("video");
+  const toggleRecordOption = (type) => {
+    return () => {
+      setRecordOption(type);
+    };
+  };
+  return (
+    <div>
+      <h1>Cam Quest</h1>
+      <div className="button-flex">
+        <button onClick={toggleRecordOption("video")}>
+          Record Video
+        </button>
+        <button onClick={toggleRecordOption("audio")}>
+          Record Audio
+        </button>
+      </div>
+      <div>
+        {recordOption === "video" ? <VideoRecorder /> : <AudioRecorder />}
+      </div>
+    </div>
+  );
+};
+export default App;
